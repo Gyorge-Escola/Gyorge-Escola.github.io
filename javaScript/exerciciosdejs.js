f24b3c3e7f5c013710df2d63f005be35ceb91e4a
gyorge-escola <gyorge.moreira@escola.pr.gov.br>
2025-09-04 11:02:23 -0300

    Auto commit em 2025-09-04 11:02:23

---
+++ javaScript/exerciciosdejs.js
@@ -0,0 +1,46 @@
+function Versin() {
+    let num = prompt("Digite um numero")
+    switch (num) {
+        case num=0:
+            console.log("Seu número é 0")
+            break
+        case num<0:
+            console.log("Seu número é menor que 0")
+            break
+        case num>0:
+            console.log("Seu número é maior que 0")
+        break
+    }
+}
+
+function Check() {
+    // Função vazia
+}
+
+function ParImpar() {
+    // Função vazia
+}
+
+function Desc() {
+    // Função vazia
+}
+
+function Nota() {
+    // Função vazia
+}
+
+function Numaior() {
+    // Função vazia
+}
+
+function Login() {
+    // Função vazia
+}
+
+function Faixa() {
+    // Função vazia
+}
+
+function Semana() {
+    // Função vazia
+}
