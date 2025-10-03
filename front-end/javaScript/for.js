d5f3a126caf00877bc5434bd68a5f99a85b5b24b
gyorge-escola <gyorge.moreira@escola.pr.gov.br>
2025-10-03 12:30:54 -0300

    Auto commit em 2025-10-03 12:30:54

--- front-end/javaScript/for.js
+++ front-end/javaScript/for.js
@@ -1,24 +1,24 @@
 function contagem(){
-    for (let i=0;i==10;i++) {
+    for (let i=0;i<=10;i++) {
         alert("Número "+i)
     }
 }
 
 function contagemr(){
-    for (let i=10;i==0;i--) {
+    for (let i=10;i<=0;i--) {
         alert("Número "+i)
     }
 }
 
 function pares(){
-    for (let i=0;i==20;i+2) {
+    for (let i=0;i<=20;i+2) {
         alert("Número "+i)
     }
 }
 
 function tabs(){
     let numero = parseInt(prompt("Digite um número"))
-    for (let i=0;i==10;i++) {
+    for (let i=0;i<=10;i++) {
         alert(numero*i)
     }
     alert(numero)
@@ -26,7 +26,7 @@ function tabs(){
 
 function cem(){
     let numero = 0
-    for (let i=0;i==100;i++) {
+    for (let i=0;i<=100;i++) {
         numero + i
         console.log(i, numero)
     }
