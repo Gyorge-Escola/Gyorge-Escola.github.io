7726c62563635aaac826632f4e2f1ec01daa769c
gyorge-escola <gyorge.moreira@escola.pr.gov.br>
2025-10-03 12:15:33 -0300

    Auto commit em 2025-10-03 12:15:33

--- front-end/javaScript/for.js
+++ front-end/javaScript/for.js
@@ -1,3 +1,11 @@
 function contagem(){
-    
+    for (let i=0;i==10;i++) {
+        alert("Número "+i)
+    }
+}
+
+function contagemr(){
+    for (let i=10;i==0;i--) {
+        alert("Número "+i)
+    }
 }
